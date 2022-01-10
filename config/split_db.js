5c0e500aa5a049a7b16a32b6c73d902651f86649
StefanoOr <Bastord@hotmail.it>
2022-01-10 13:23:47 +0100

    upload query insert

--- config/split_db.js
+++ config/split_db.js
@@ -35,6 +35,7 @@ const setActor = async function (_actor) {
         console.log("table_model", _actor)
 
         const id = _actor.id;
+
         const name = _actor.name;
         const producer = _actor.producer_id
         const query = "INSERT INTO actor(id,name, producer_id) VALUES(?,?,?)"
@@ -55,8 +56,10 @@ const setActor = async function (_actor) {
 
 const setActorGroup = async function (_actorGroup) {
     try {
+      const id = _actorGroup.id;
+      const actor_id = _actorGroup.actor_id;
         const query = "INSERT INTO actor_group(id,actor_id) VALUES(?,?)"
-        const [rows, fields] = await (await connection).execute(query, [ _actorGroup.id, _actorGroup.actor_id] );
+        const [rows, fields] = await (await connection).execute(query, [ id, actor_id] );
         console.log("rows", rows)
         console.log("fields", fields)
 
@@ -68,8 +71,13 @@ const setActorGroup = async function (_actorGroup) {
 
 const setEnum = async function (_enum) {
     try {
-        const query = "INSERT INTO enum(id,value_num,value_string,recorsiveNum) VALUES(?,?)"
-        const [rows, fields] = await (await connection).execute(query, [ _enum.id, _enum.value_num,_enum.value_string,_enum.recorsiveNum] );
+    const id= _enum.id;
+    const value_num= _enum.value_num;
+    const value_string = _enum.value_string;
+    const recorsiveNum= _enum.recorsiveNum;
+
+        const query = "INSERT INTO enum(id,value_num,value_string,recorsiveNum) VALUES(?,?,?,?)"
+        const [rows, fields] = await (await connection).execute(query, [id,value_num,value_string,recorsiveNum] );
         console.log("rows", rows)
         console.log("fields", fields)
 
@@ -81,9 +89,18 @@ const setEnum = async function (_enum) {
 
 const setEvent = async function (_event) {
     try {
-        const query = "INSERT INTO event (id,name,description,resource,output_product,Kfact,actor_group,help) VALUES(?,?)"
-        const [rows, fields] = await (await connection).execute(query, [ _event.id, _event.name,_event.description,
-            _event.resource,_event.output_product,_event.Kfact,_event.actor_group,_event.help] );
+
+    const id= _event.id;
+    const name = _event.name;
+    const description = _event.description;
+    const resource = _event.resource;
+    const output_product = _event.output_product;
+    const kFact = _event.Kfact;
+    cost actor_group = _event.actor_group;
+    const help = _event.help;
+
+        const query = "INSERT INTO event (id,name,description,resource,output_product,Kfact,actor_group,help) VALUES(?,?,?,?,?,?,?,?)"
+        const [rows, fields] = await (await connection).execute(query, [ id,name,description,resource,output_product,Kfact,actor_group,help] );
         console.log("rows", rows)
         console.log("fields", fields)
 
@@ -95,9 +112,23 @@ const setEvent = async function (_event) {
 
 const setEventResult = async function (_eventResult) {
     try {
-        const query = "INSERT INTO event_result (id,name,description,quantity,product_id,type,actor_id,product_detail_id,datetime,id_event,producer_id,operator_id) VALUES(?,?)"
-        const [rows, fields] = await (await connection).execute(query, [ _eventResult.id, _eventResult.name,_eventResult.description,
-            _eventResult.quantity,_eventResult.product_id,_eventResult.type,_eventResult.actor_id,_eventResult.product_id,_eventResult.product_id,_eventResult.operator_id] );
+
+    const id = _eventResult.id;
+    const name = _eventResult.name;
+    const description = _eventResult.description;
+    const quantity = _eventResult.quantity;
+    const product_id = _eventResult.product_id;
+    const type = _eventResult.type;
+    const actor_id = _eventResult.actor_id;
+    const product_detail_id = _eventResult.product_detail_id;
+    const datetime = _eventResult.datetime;
+    const id_event = _eventResult.id_event;
+    const producer_id = _eventResult.producer_id;
+    const operator_id = _eventResult.operator_id;
+
+        const query = "INSERT INTO event_result (id,name,description,quantity,product_id,type,actor_id,product_detail_id,datetime,id_event,producer_id,operator_id) VALUES(?,?,?,?,?,?,?,?,?,?,?,?)"
+        const [rows, fields] = await (await connection).execute(query, [ id,name,description,quantity,product_id,type,actor_id,product_detail_id,datetime,id_event,
+        producer_id,operator_id] );
         console.log("rows", rows)
         console.log("fields", fields)
 
@@ -109,9 +140,21 @@ const setEventResult = async function (_eventResult) {
 
 const setOperator = async function (_operator) {
     try {
-        const query = "INSERT INTO operator (id,name,surname,address,user,password,producer_id,type,sup_op) VALUES(?,?)"
-        const [rows, fields] = await (await connection).execute(query, [ _operator.id, _operator.name,_operator.surname,
-            _operator.address,_operator.user,_operator.password,_operator.product_id,_operator.type,_operator.sup_op] );
+
+    const id = _operator.id;
+    const name = _operator.name;
+    const surname = _operator.surname;
+    const address = _operator.address;
+    const user = _operator.user;
+    const password = _operator.password;
+    const producer_id = _operator.producer_id;
+    const type = _operator.type;
+    const sup_op = _operator.sup_op;
+
+
+
+        const query = "INSERT INTO operator (id,name,surname,address,user,password,producer_id,type,sup_op) VALUES(?,?,?,?,?,?,?,?,?)"
+        const [rows, fields] = await (await connection).execute(query, [ id,name,surname,address,user,password,producer_id,type,sup_op] );
         console.log("rows", rows)
         console.log("fields", fields)
 
@@ -124,9 +167,16 @@ const setOperator = async function (_operator) {
 
 const setParameter = async function (_parameter) {
     try {
-        const query = "INSERT INTO parameter (id,name,mandatory,type,detail,event_id) VALUES(?,?)"
-        const [rows, fields] = await (await connection).execute(query, [ _parameter.id, _parameter.name,_parameter.mandatory,
-            _parameter.type,_parameter.detail,_parameter.event_id] );
+
+    const id = _parameter.id;
+    const name = _parameter.name;
+    const mandatory = _parameter.mandatory;
+    const type = _parameter.type;
+    const detail = _parameter.detail;
+    const event_id = _parameter.event_id;
+
+        const query = "INSERT INTO parameter (id,name,mandatory,type,detail,event_id) VALUES(?,?,?,?,?,?)"
+        const [rows, fields] = await (await connection).execute(query, [ id,name,mandatory,type,detail,event_id] );
         console.log("rows", rows)
         console.log("fields", fields)
 
@@ -138,10 +188,21 @@ const setParameter = async function (_parameter) {
 
 const setParameteDetail = async function (_parameterDetail) {
     try {
-        const query = "INSERT INTO parameter_detail (id,name,mandatory,type,detail,event_id) VALUES(?,?)"
-        const [rows, fields] = await (await connection).execute(query, [ _parameterDetail.id,_parameterDetail.val_min,_parameterDetail.val_max,
-            _parameterDetail.dim_max,_parameterDetail.fixed,_parameterDetail.help_param,_parameterDetail.hash_type,_parameterDetail.url_upload,
-            _parameterDetail.photo_upload,_parameterDetail.enum_id] );
+
+    const id =_parameterDetail.id;
+    const val_min = _parameterDetail.val_min;
+    const val_max = _parameterDetail.type;
+    const dim_max = _parameterDetail.detail;
+    const fixed = _parameterDetail.fixed;
+    const help_param = _parameterDetail.help_param;
+    const hash_type = _parameterDetail.hash_type;
+
+    const url_upload = _parameterDetail.url_upload;
+    const photo_upload = _parameterDetail.photo_upload;
+    const enum_id = _parameterDetail.enum_id;
+
+        const query = "INSERT INTO parameter_detail (id,val_min,val_max,dim_max,fixed,help_param,hash_type,url_upload,photo_upload,enum_id) VALUES(?,?,?,?,?,?,?,?,?,?)"
+        const [rows, fields] = await (await connection).execute(query, [ id,val_min,val_max,dim_max,fixed,help_param,hash_type,url_upload,photo_upload,enum_id] );
         console.log("rows", rows)
         console.log("fields", fields)
 
@@ -154,8 +215,14 @@ const setParameteDetail = async function (_parameterDetail) {
 
 const setProducer = async function (_producer) {
     try {
-        const query = "INSERT INTO producer (id,name,description,website) VALUES(?,?)"
-        const [rows, fields] = await (await connection).execute(query, [ _producer.id,_producer.name,_producer.description,_producer.website] );
+
+    const id = _producer.id;
+    const name = _producer.name;
+    const description = _producer.description;
+    const website = _producer.website;
+
+        const query = "INSERT INTO producer (id,name,description,website) VALUES(?,?,?,?)"
+        const [rows, fields] = await (await connection).execute(query, [ id,name,description,website] );
         console.log("rows", rows)
         console.log("fields", fields)
 
@@ -167,8 +234,14 @@ const setProducer = async function (_producer) {
 
 const setProduct = async function (_product) {
     try {
-        const query = "INSERT INTO product (id,min_time_intervall,producer_id,finish) VALUES(?,?)"
-        const [rows, fields] = await (await connection).execute(query, [ _product.id,_product.min_time_intervall,_product.producer_id,_product.finish] );
+
+    const id = _product.id;
+    const min_time_intervall = _product.min_time_intervall;
+    const producer_id = _product.producer_id;
+    const finish = _product.finish;
+
+        const query = "INSERT INTO product (id,min_time_intervall,producer_id,finish) VALUES(?,?,?,?)"
+        const [rows, fields] = await (await connection).execute(query, [id,min_time_intervall,producer_id,finish] );
         console.log("rows", rows)
         console.log("fields", fields)
 
@@ -182,9 +255,19 @@ const setProduct = async function (_product) {
 
 const setProductDetail = async function (_productDetail) {
     try {
-        const query = "INSERT INTO product_detail (id,name,description,quantity,type,unit_measure,product_id,residualPerc) VALUES(?,?)"
-        const [rows, fields] = await (await connection).execute(query, [ _productDetail.id,_productDetail.name,_productDetail.description,
-            _productDetail.quantity,_productDetail.type,_productDetail.unit_measure,_productDetail.product_id,_productDetail.residualPerc] );
+
+    const id = _productDetail.id;
+    const name = _productDetail.name;
+    const description = _productDetail.description;
+    const quantity = _productDetail.quantity;
+    const type = _productDetail.type;
+    const unit_measure = _productDetail.unit_measure;
+    const product_id = _productDetail.product_id;
+    const residualPerc = _productDetail.residualPerc;
+
+
+        const query = "INSERT INTO product_detail (id,name,description,quantity,type,unit_measure,product_id,residualPerc) VALUES(?,?,?,?,?,?,?,?)"
+        const [rows, fields] = await (await connection).execute(query, [ id,name,description,quantity,type,unit_measure,producer_id,residualPerc] );
         console.log("rows", rows)
         console.log("fields", fields)
 
