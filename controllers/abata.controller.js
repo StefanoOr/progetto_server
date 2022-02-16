f8d0836b1206ee44283ae6c4fde3c4a0ef4c506a
StefanoOr <Bastord@hotmail.it>
2022-02-16 14:17:07 +0100

    aggiornamento Login , changePassword, recoveryPass

--- controllers/abata.controller.js
+++ controllers/abata.controller.js
@@ -2,29 +2,56 @@
 const express = require("express");
 const connection = require("../models/db");
 
- 
+exports.getPassword=async function(req,res,next){
+    console.log("recovery password");
+
+    const user=req.body.username;
+
+    const query = "SELECT password FROM operator   WHERE user=?  ";
+    const [rows, fields] =  await (await connection).execute(query, [user] );
+    console.log("rows", rows);
+    res.end(JSON.stringify({ id: rows}));
+}
+
+
+ exports.changePassword= async function(req,res,next){
+    console.log("ChangePassword");
+    const user = req.body.username;
+    const password = req.body.password;
+    const nuovapassword = req.body.nuovapassword;
+    
+    const query = "UPDATE operator SET password=?  WHERE user=? AND password=? ";
+
+    const [rows, fields] =  await (await connection).execute(query, [nuovapassword,user ,password] );
+    
+    res.end(JSON.stringify({ id: rows.info}));
+ }
 
 exports.basicLogin= async function (req,res,next){
     console.log("Login ");
 
     const ciccio = req.body.username;
     const password1 = req.body.password;
-    	
-console.log(ciccio);
-        const query = "SELECT user, password FROM operator WHERE user=? AND password=? ";
+    
+
+        const query = "SELECT id FROM operator WHERE user=? AND password=? ";
         /*query è la query come la scriveresti in sql
         * i valori sono sostituiti da un ?
         * in ordine, il primo preleva l'id
         * il secondo il nome
         * il terzo il producer*/
-       console.log(password1);
-        const [rows, fields] =  await (await connection).execute(query, [ciccio ,password1] );
+      
+        const [rows] =  await (await connection).execute("SELECT id FROM operator WHERE user=? AND password=? ", [ciccio ,password1] );
+      
+            
         console.log("rows", rows);
-        console.log("fields", fields);
-
+        //console.log("fields", colums);
+     
+       console.log("id", id);
+        res.setHeader('Title', 'value');
+        res.end(JSON.stringify({ id: rows}));
     
-    res.setHeader('Title', 'value');
-    res.end(JSON.stringify({ username: ciccio, password: password1 })); //non stai inviando il risultato della query e non stai verificando se user e password sono presenti,
+     //non stai inviando il risultato della query e non stai verificando se user e password sono presenti,
                                                                             //fai la query ma ne ignori il risultato, infatti rendi sempre quello che mandi da postman
 
 
