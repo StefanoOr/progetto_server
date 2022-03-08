a39f9f14a6d5ac0d244566495f88a9c05f1dde9e
StefanoOr <Bastord@hotmail.it>
2022-03-08 12:42:22 +0100

    Aggiornamento login

--- controllers/abata.controller.js
+++ controllers/abata.controller.js
@@ -1,8 +1,6 @@
-// qui devi creare il modulo rischiamato dal route
-const express = require("express"); // non lo usi, si cancella
-const connection = require("../models/db"); //non lo usi, si cancella
-const Abata = require("../models/abata.model"); // non esiste, di conseguenza le funzioni  Abata.login
-                                                // e Abata.insertAddress non ci sono
+
+
+const Abata = require("../models/abata.model"); 
 
 
 exports.serverOn= (req, res) => {
@@ -12,26 +10,26 @@ exports.serverOn= (req, res) => {
 
 exports.basicLogin= (req,res,next)=> {
     console.log("dati inseriti su postman",req.body);
-    if (!(req.body.username && req.body.password && req.body.address && req.body.id)) { //controllo sicuramente migliorabile, si verifica quando tutti sono vuoti
-        res.status(400).send(
+    if (!(req.body.username && req.body.password && req.body.address && req.body.id)) { 
+        res.status(401).send(
             {message: "I campi non possono essere vuoti"});
             return;
       }
     
     
 
-      Abata.login(req.body.id,req.body.username,req.body.password,(err,data)=>{ // scelta non corretta per chiamare la funzione
-        // La via più semplice è usare il modo usato dall'inizio, simulando la programmazione sincrona con await
+      Abata.login(req.body.id,req.body.username,req.body.password,(err,data)=>{ 
+    
         console.log(err,"dati",data);
-          if(err){ //se è un'errore non significa che non lo trova
-            
-                res.status(500).send({
+          if(err== 401){ //se è un'errore non significa che non lo trova
+         
+                res.status(401).send({
                   message: "Operatore non trovato con id , username  o password sbagliata O ADDRESS gia inserito "
                 });
              
-              }else{
+              }else {
 
-                Abata.insertAddress(req.body.id,req.body.username,req.body.password,req.body.address,(err,data)=>{
+                Abata.insertAddress(req.body.id,req.body.username,req.body.password,req.body.address,(err)=>{
                     if(err){
                         res.status(400).send({
                             message: "errore nell'inserimento dell'address "
@@ -50,4 +48,4 @@ exports.basicLogin= (req,res,next)=> {
     }
     
     
- 
+
--- models/abata.model.js
+++ models/abata.model.js
@@ -1,52 +1,47 @@
+const connection = require("../models/db");
 
-const connection = require("../models/db");;
-
-
-
-exports.login=async (id,username,password,result)=>{
+login= async (id,username,password,result)=>{
     // qui non usi try e catch, inoltre fai la query, ma usi return (nulla) al chiamante  solamente quando non trovi corrispondenza
     console.log(id,password,username);
+    let rows;
+    try{
     const query = "SELECT user,password,address FROM operator WHERE user=? AND password=? AND id=? AND address IS NULL";
         
-        const [rows] = await (await connection).execute(query, [username, password, id] ); //Esempio   simulando la programmazione sincrona
-    
-        console.log(rows);
+         [rows] = await (await connection).execute(query, [username, password, id] ); //Esempio   simulando la programmazione sincrona
+         console.log("dati della query login",rows);
+        
+    }catch(err){
+        console.log("login catch",err);
+        res.status(400).send("errore login 400");
+       // result(400, null);
+        return;
+    }
+       
         if (rows.length  == 0){
-            
-            result(true, null);
+            result(401, null);
             return;
           }
-
-
         
-        result(null,rows);
+        result(200,rows);
 
 };
 
 
-exports.insertAddress = async (id,username,password,address) =>{
+insertAddress = async (id,username,password,address) =>{
    
     try{
     await (await connection).execute("UPDATE operator SET address=?  WHERE user=? AND password=? AND id=?", [address, username, password, id]);
 
-
     console.log("address inserito con successo");
-    result(false,null);  //result non c'è in questa funzione
     return;
 
     }catch(err){
-        console.log("errore nell'inserimento dell'addresss");
-        result(true,null); //result non c'è in questa funzione
+        console.log("errore nell'inserimento dell'addresss",err);
+        result(err);
         return;
     }
     
              
-    
-    
-
-    
-
-
 }
 
-
+module.exports ={login,insertAddress};
